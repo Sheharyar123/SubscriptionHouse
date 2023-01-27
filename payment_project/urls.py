0aebf38d2325a563aba886bf24a68168c0b8c75b
Sheharyar Ahmad <phantomdjangouser@gmail.com>
2023-01-27 21:31:18 +0500

    secure_proxy_ssl_header and allowed_hosts update

--- payment_project/urls.py
+++ payment_project/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("subscriptions-house-admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("products.urls")),
     path("paypal/", include("paypal.standard.ipn.urls")),
